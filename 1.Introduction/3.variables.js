7513f6dd80d874597550fdb4691c8969f8b1b868
Satya <satyaganesh987@gmail.com>
2022-01-24 11:38:59 +0530

    2022-01-24: added variable declaration

---
+++ 1.Introduction/3.variables.js
@@ -0,0 +1,5 @@
+var studentName = "Satya";
+var maths = 24;
+var avg = 25.3;
+var pass = true;
+console.log("Report => " + studentName + " : " + pass);
---
+++ 1.Introduction/3.variables.ts
@@ -0,0 +1,6 @@
+let studentName: string = "Satya"
+let maths: number = 24
+let avg: number = 25.3
+let pass: boolean = true
+
+console.log("Report => " + studentName + " : " + pass)
