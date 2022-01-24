229f24a281e0365418086ce10ab1d783734466e6
Satya <satyaganesh987@gmail.com>
2022-01-24 11:34:23 +0530

    2022-01-24: added difference between let var and const

---
+++ 1.Introduction/2.LetVarConst.ts
@@ -0,0 +1,22 @@
+var greetings : string = "good morning"
+var greetings = "good evening" //redeclarable
+greetings = "bye" //updatabale
+
+let abc = "abc"
+let abc = 'def' // cant redeclare
+abc = 'def' // can update
+
+
+var time = 10
+function greet() {
+    if (time < 20) {
+        var treet = "URU brewery"
+        let count = 15
+    }
+    console.log(treet) // treet has function scope, so it is available
+    console.log(count) // count is block scope so it is not available
+}
+
+// var -> global/ nearest function scope, updatable, redeclarable
+// let -> block {} scope
+// const -> block scope
