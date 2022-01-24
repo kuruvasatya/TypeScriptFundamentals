e473717f0d8c5f1c3267226f5fafa85688c1df0b
Satya <satyaganesh987@gmail.com>
2022-01-24 12:38:53 +0530

    2022-01-24: added basic class with only fields

---
+++ 2.Classes/1.Customer.js
@@ -0,0 +1,10 @@
+var Customer1 = /** @class */ (function () {
+    function Customer1() {
+    }
+    return Customer1;
+}());
+var customer1 = new Customer1();
+customer1.firstName = "satya";
+customer1.lastName = "Ganesh";
+console.log(customer1.firstName);
+console.log(customer1.lastName);
---
+++ 2.Classes/1.Customer.ts
@@ -0,0 +1,11 @@
+class Customer1 {
+    firstName: string;
+    lastName: string;
+}
+
+let customer1 = new Customer1();
+customer1.firstName = "satya";
+customer1.lastName = "Ganesh";
+
+console.log(customer1.firstName);
+console.log(customer1.lastName);
